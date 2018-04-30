99eda480b64618ffff4ebf8fa590e53b4df6f880
Nofrets Poai <opetstudio@gmail.com>
2018-04-30 21:49:32 +0700

    edit contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -26,6 +26,10 @@ class ContactPage extends Component {
                 <p style={{ fontSize: '1.33em' }}>
                   Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
                 </p>
+                <Header as='h3' style={{ fontSize: '2em' }}>We Make Bananas That Can Dance</Header>
+                <p style={{ fontSize: '1.33em' }}>
+                  Yes that's right, you thought it was the stuff of dreams, but even bananas can be bioengineered.
+                </p>
               </Grid.Column>
               <Grid.Column floated='right' width={6}>
                 <Image
